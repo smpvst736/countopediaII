013ca29e5103928ac48bf2d901c69bae0fe70b6d
David <krumholz.david@outlook.com>
2026-03-02 10:36:26 -0500

    Third commit changes

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
+
 function Counter() {
-  let count = 0;
+  let [count, setCount] = useState(0);
+
+  function handleIncrement() {
+    setCount((count += 1));
+    console.log(count);
+  }
+
+  function handleDecrement() {
+    setCount((count -= 1));
+    console.log(count);
+  }
+
   return (
     <div className="row text-white container">
       Count: {count}
-      <button className="btn btn-success m-2">+1</button>
-      <button className="btn btn-danger m-2">-1</button>
+      <button onClick={handleIncrement} className="btn btn-success m-2">
+        +1
+      </button>
+      <button onClick={handleDecrement} className="btn btn-danger m-2">
+        -1
+      </button>
     </div>
   );
 }
